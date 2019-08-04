6d55af1405b33e1ba4713e04e0b83fd8dc4570a9
ppuczka <p.puczka@gmail.com>
2019-08-04 17:57:21 +0200

    first commit -homegrownmarket project created

---
+++ src/main/java/com/devproject/homegrownmarket/HomegrownmarketApplication.java
@@ -0,0 +1,13 @@
+package com.devproject.homegrownmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomegrownmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomegrownmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devproject/homegrownmarket/HomegrownmarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devproject.homegrownmarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HomegrownmarketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
