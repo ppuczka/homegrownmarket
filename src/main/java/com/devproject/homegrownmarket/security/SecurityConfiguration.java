46a8a2574326242d6c1f396a726e3364f1897a4c
p.puczka <Warchlak77>
2019-09-10 23:01:25 +0200

    started spring security implementation

---
+++ src/main/java/com/devproject/homegrownmarket/security/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.devproject.homegrownmarket.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("adminPass").roles("ADMIN")
+                .and()
+                .withUser("user").password("userPass").roles("USER");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("app/").authenticated()
+                .antMatchers("/app/admin/**").hasRole("ADMIN")
+                .antMatchers("/").access()
+                .and()
+                .formLogin()
+                .successHandler(appSucessHandler)
+                .failureHandler(appFailureHandler)
+                .and()
+                .logout();
+    }
+
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
